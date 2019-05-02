e45d0f7d60a3b6266a038a166b1aa6eae1763088
Pablo Mora <pmora1990@gmail.com>
2019-05-02 07:29:33 -0600

    random fill & blinking text

--- js/app.js
+++ js/app.js
@@ -15,6 +15,45 @@ $(document).ready(function(){
     });
   });
 
+
+  function startBlinking(){
+    setInterval(function () {
+      blink();
+    }, 1000);
+  }
+  function blink() {
+      $(".main-titulo").css("color", "white");
+    setTimeout(function(){
+      $(".main-titulo").css("color", "#DCFF0E");
+    },500);
+  }
+  startBlinking();
+
+
+
+  // setTimeout(function(){
+  //   $(".main-titulo").css("color", "white");
+  // },2000);
+
+
+  /*setTimeout(function(){
+    $(".main-titulo").css("color", "white");
+    setTimeout(function(){
+      $(".main-titulo").css("color", "#DCFF0E");
+    },2000);
+  },2000);*/
+
+  /*function blink(){
+    setTimeout(function(){
+      $(".main-titulo").css("color", "white");
+    },2000);
+    setTimeout(function(){
+      $(".main-titulo").css("color", "#DCFF0E");
+    },2000);
+    blink();
+  };*/
+  //blink();
+
   var list = ['0', '1', '2', '3', '4', '5', '6'];
 
   for(i = 0; i < list.length; i++){
@@ -22,7 +61,9 @@ $(document).ready(function(){
       $(".col-" + (i+1).toString()).append("<img id='theImg' src=image/" + (Math.floor((Math.random() * 4) + 1)).toString() + ".png/>");
     }
   }
-  $(":image").css("width", "80%");
+  $("img").attr("width", "76%");
+
+  // setTime();
   //<img id='theImg' src=image/" + (Math.floor((Math.random() * 4) + 1)).toString() + ".png/>
   /*
   //Create rows. Append rows to columns
