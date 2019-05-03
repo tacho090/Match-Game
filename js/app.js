2cf7f9ef241b88d21a4d481be591880bf83787eb
Pablo Mora <pmora1990@gmail.com>
2019-05-02 20:15:10 -0600

    detach and prepend function programmed

--- js/app.js
+++ js/app.js
@@ -33,7 +33,7 @@ $(document).ready(function(){
 
   for(i = 0; i < list.length; i++){
     for(j=0; j < list.length; j++){
-      $(".col-" + (i+1).toString()).append("<img id='theImg' src=image/" + (Math.floor((Math.random() * 4) + 1)).toString() + ".png/>");
+      $(".col-" + (i+1).toString()).append("<img id='theImg' src='image/" + (Math.floor((Math.random() * 4) + 1)).toString() + ".png/'>");
     }
   }
 
@@ -46,28 +46,40 @@ $(document).ready(function(){
     });
   });
 
+  var detach_1, detach_2, detach_3;
+
   setTimeout(function(){
     for(var j = 1 ; j<8; j++){
       for(var i = 2; i<7; i++){
-        console.log($("div[class ^= 'col-" + j.toString() + "'] img:nth-child(" + i.toString() + ")"));
-        if($("div[class ^= 'col-" + j.toString() + "'] img:nth-child(" + i.toString() + ")").attr("src") == $("div[class ^= 'col-" + j.toString() + "'] img:nth-child(" + (i+1).toString() + ")").attr("src") && $("div[class ^= 'col-" + j.toString() + "'] img:nth-child(" + i.toString() + ")").attr("src") == $("div[class ^= 'col-" + j.toString() + "'] img:nth-child(" + (i-1).toString() + ")").attr("src")){
-          $("div[class ^= 'col-" + j.toString() + "'] img:nth-child(" + i.toString() + ")").hide(2000);
-          $("div[class ^= 'col-" + j.toString() + "'] img:nth-child(" + (i+1).toString() + ")").hide(2000);
-          $("div[class ^= 'col-" + j.toString() + "'] img:nth-child(" + (i-1).toString() + ")").hide(2000);
-          console.log("match");
+        //console.log($(".col-" + j.toString() + " img:nth-child(" + i.toString() + ")"));
+        if($(".col-" + j.toString() + " img:nth-child(" + i.toString() + ")").attr("src") == $(".col-" + j.toString() + " img:nth-child(" + (i+1).toString() + ")").attr("src") && $(".col-" + j.toString() + " img:nth-child(" + i.toString() + ")").attr("src") == $(".col-" + j.toString() + " img:nth-child(" + (i-1).toString() + ")").attr("src")){
+          detach_1 = $(".col-" + j.toString() + " img:nth-child(" + i.toString() + ")").hide(2000);
+          detach_2 = $(".col-" + j.toString() + " img:nth-child(" + (i+1).toString() + ")").hide(2000);
+          detach_3 = $(".col-" + j.toString() + " img:nth-child(" + (i-1).toString() + ")").hide(2000);
+          setTimeout(function(){
+            detach_1.detach();
+            detach_2.detach();
+            detach_3.detach()
+          }, 2000);;
         }
       }
     }
   },2000);
 
-  /*for(var i = 2; i<7; i++){
-    console.log($("div[class ^= 'col-1'] img:nth-child(" + i.toString() + ")"));
-    if($("div[class ^= 'col-1'] img:nth-child(" + i.toString() + ")").attr("src") == $("div[class ^= 'col-1'] img:nth-child(" + (i+1).toString() + ")").attr("src") && $("div[class ^= 'col-1'] img:nth-child(" + i.toString() + ")").attr("src") == $("div[class ^= 'col-1'] img:nth-child(" + (i-1).toString() + ")").attr("src")){
-      $("div[class ^= 'col-1'] img:nth-child(" + i.toString() + ")").detach();
-      $("div[class ^= 'col-1'] img:nth-child(" + (i+1).toString() + ")").detach();
-      $("div[class ^= 'col-1'] img:nth-child(" + (i-1).toString() + ")").detach();
-      console.log("match");
+
+  setTimeout(function(){
+    for(var i = 1; i<8; i++){
+      console.log('column number ' + i.toString() + ' passed');
+      if($(".col-" + i.toString()).children().length < 7){
+        console.log('column number ' + i.toString() + ' passed');
+        let difference = 7 - ($(".col-" + i.toString()).children().length);
+        for(var j = 0; j<difference; j++){
+          // console.log('amount of children of col-' + i.toString());
+          $('.col-' + i.toString()).prepend("<img id='theImg' src='image/" + (Math.floor((Math.random() * 4) + 1)).toString() + ".png/'>");
+        }
+      }
     }
-  }*/
+    $("img").attr({"width":"76%", "height":"97px"});
+  }, 5000);
 
 });
