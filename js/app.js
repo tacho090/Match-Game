50b26b675bfcf0ab5b19872cb6ff16cc867c92bc
Pablo Mora <pmora1990@gmail.com>
2019-05-07 11:22:27 -0600

    detect coincidences on rows

--- js/app.js
+++ js/app.js
@@ -37,7 +37,7 @@ $(document).ready(function(){
     $("div[class ^= 'col-']").attr({"style":"height:680px"});
     for(i = 0; i < list.length; i++){
       for(j=0; j < list.length; j++){
-        $(".col-" + (i+1).toString()).append("<img id='draggable' src='image/" + (Math.floor((Math.random() * 4) + 1)).toString() + ".png/'>");
+        $(".col-" + (i+1).toString()).append("<img src='image/" + (Math.floor((Math.random() * 4) + 1)).toString() + ".png/'>");
       }
     }
     $("img").attr({"width":"76%", "height":"97px"});
@@ -78,7 +78,7 @@ $(document).ready(function(){
     });
   });
 
-  var detach_1, detach_2, detach_3;
+  var detach_col_1, detach_col_2, detach_col_3, detach_row_1, detach_row_2, detach_row_3;
 
   function pulse(div0, div1, div2){
     function startpulse(){
@@ -104,31 +104,70 @@ $(document).ready(function(){
 
   function detach_function(list){
     for(var i = 0; i<list.length; i++){
-      list[i].detach();
+      console.log(list[i]);
+      list[i].hide( 5000 ,function(){
+        console.log("Animation complete");
+      });
     }
+    // for(var i = 0; i<list.length; i++){
+    //   //list[i].detach();
+    //   $(list[i]).animate({
+    //     opacity: 0.25,
+    //     height: "hide"
+    //   }, 2000, function() {
+    //     // Animation complete.
+    //   });
+    // }
   }
 
+  //encontrar coincidencias en filas
+  $(function(){
+    let detach_row_list = [];
+    setTimeout(function(){
+      for(var i = 1; i<8; i++){//filas
+        for(var j = 2; j<7; j++){//columnas
+          if($(".col-" + j.toString() + "img:nth-child(" + i.toString() + ")").attr("src") === $(".col-" + (j+1).toString() + "img:nth-child(" + i.toString() + ")").attr("src") && $(".col-" + j.toString() + "img:nth-child(" + i.toString() + ")").attr("src") === $(".col-" + (j-1).toString() + "img:nth-child(" + i.toString() + ")").attr("src")){
+            detach_row_1 = $(".col-" + j.toString() + " img:nth-child(" + i.toString() + ")");
+            detach_row_2 = $(".col-" + (j+1).toString() + " img:nth-child(" + i.toString() + ")");
+            detach_row_3 = $(".col-" + (j-1).toString() + " img:nth-child(" + i.toString() + ")");
+            detach_row_list.push(detach_row_1, detach_row_2, detach_row_3);
+          }else{
+            console.log("no coincidence");
+          }
+        }
+      }
+    }, 1000);
+    console.log("*".repeat(25) + "Rows" + "*".repeat(25));
+    console.log(detach_row_list);
+    console.log("*".repeat(50));
+  });
+
+
+  //encontrar coincidencias en columnas
   $(function(){
     //let detach_1, detach_2, detach_3;
-    let detach_list1 = [];
+    let detach_column_list1 = [];
     let detach_list2 = [];
     let detach_list3 = [];
     setTimeout(function(){
       for(var j = 1 ; j<8; j++){
         for(var i = 2; i<7; i++){
           if($(".col-" + j.toString() + " img:nth-child(" + i.toString() + ")").attr("src") == $(".col-" + j.toString() + " img:nth-child(" + (i+1).toString() + ")").attr("src") && $(".col-" + j.toString() + " img:nth-child(" + i.toString() + ")").attr("src") == $(".col-" + j.toString() + " img:nth-child(" + (i-1).toString() + ")").attr("src")){
-            detach_1 = $(".col-" + j.toString() + " img:nth-child(" + i.toString() + ")");
-            detach_2 = $(".col-" + j.toString() + " img:nth-child(" + (i+1).toString() + ")");
-            detach_3 = $(".col-" + j.toString() + " img:nth-child(" + (i-1).toString() + ")");
-            detach_list1.push(detach_1, detach_2, detach_3);
-            pulse(detach_1, detach_2, detach_3);
+            detach_col_1 = $(".col-" + j.toString() + " img:nth-child(" + i.toString() + ")");
+            detach_col_2 = $(".col-" + j.toString() + " img:nth-child(" + (i+1).toString() + ")");
+            detach_col_3 = $(".col-" + j.toString() + " img:nth-child(" + (i-1).toString() + ")");
+            detach_column_list1.push(detach_col_1, detach_col_2, detach_col_3);
+            pulse(detach_col_1, detach_col_2, detach_col_3);
           }
         }
       }
       setTimeout(function(){
-        detach_function(detach_list1);
-      }, 5000);
+        detach_function(detach_column_list1);
+      }, 3000);
     },1000);
+    console.log("*".repeat(25) + "Columns" + "*".repeat(25));
+    console.log(detach_column_list1);
+    console.log("*".repeat(50));
   });
 
   setTimeout(function(){
@@ -188,7 +227,7 @@ $(document).ready(function(){
         number_0 = '00';
       }
     });
-    timer.start();
+    // timer.start();
   });
   //
 
